433a6d5d75fe3a78fd0c182cc68ad8f88338d979
Mel Boyce <weird@syngin.net>
2017-06-16 15:39:48 +1000

    removed

--- build_scripts/vultr/docker-ubuntu.sh
+++
@@ -1,8 +0,0 @@
-apt -y update
-apt -y upgrade
-apt -y install apt-transport-https ca-certificates curl gnupg
-
-echo "deb https://apt.dockerproject.org/repo ubuntu-`lsb_release -cs` main" > /etc/apt/sources.list.d/docker.list
-apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
-apt -y update
-apt -y install docker-engine
